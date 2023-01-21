7978de060a015781ad6b5599921479c9a3fe83a3
Richie Wahidin <richie@cs.utexas.edu>
2023-01-21 17:43:40 -0600

    another commit

--- Collatz.cpp
+++ Collatz.cpp
@@ -7,8 +7,10 @@
 // --------
 
 #include <cassert> // assert
+#include <algorithm>
 
 #include "Collatz.hpp"
+
 
 using namespace std;
 
@@ -16,12 +18,27 @@ using namespace std;
 // max_cycle_length
 // ----------------
 
+int cycle_length (int n) {
+    assert(n > 0);
+    int c = 1;
+    while (n > 1) {
+        if ((n % 2) == 0)
+            n = (n / 2);
+        else
+            n = (3 * n) + 1;
+        ++c;}
+    assert(c > 0);
+    return c;}
+
 unsigned max_cycle_length (unsigned i, unsigned j) {
     assert(i > 0);
     assert(j > 0);
-
+    unsigned result = 0;
     // <your code>
-    unsigned v = i + j; // replace!
+    for (unsigned k = i; k <= j; k++) {
+        result = max<unsigned>(result, cycle_length(k));
+    }
+    assert(result > 0);
+    return result;}
 
-    assert(v > 0);
-    return v;}
+
--- TestCollatz.cpp
+++ TestCollatz.cpp
@@ -20,13 +20,13 @@ using namespace std;
 // ----------------
 
 TEST(CollatzFixture, max_cycle_length_0) {
-    ASSERT_EQ(max_cycle_length(1, 10), 11u);}
+    ASSERT_EQ(max_cycle_length(1, 1), 1u);}
 
 TEST(CollatzFixture, max_cycle_length_1) {
-    ASSERT_EQ(max_cycle_length(100, 200), 300u);}
+    ASSERT_EQ(max_cycle_length(1, 10), 20u);}
 
 TEST(CollatzFixture, max_cycle_length_2) {
-    ASSERT_EQ(max_cycle_length(201, 210), 411u);}
+    ASSERT_EQ(max_cycle_length(201, 210), 89u);}
 
 TEST(CollatzFixture, max_cycle_length_3) {
-    ASSERT_EQ(max_cycle_length(900, 1000), 1900u);}
+    ASSERT_EQ(max_cycle_length(900, 1000), 174u);}
